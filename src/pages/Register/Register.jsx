14fe390658c714542465c86319cc8fa36293042d
Hazem Abd El-Rahman <hazem.abdo1572001@gmail.com>
2025-06-01 13:36:39 +0300

    Deleted Previous Non-Functional Role Logic

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,23 +1,6 @@
-import { SignUp, useUser } from "@clerk/clerk-react";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { SignUp } from "@clerk/clerk-react";
 
 export default function Register() {
-  const { isSignedIn, user } = useUser();
-  console.log("User in register page:", user);
-  console.log("isSignedIn in register page:", isSignedIn);
-  const navigate = useNavigate();
-
-  // Assign 'user' role to new signups
-  useEffect(() => {
-    if (isSignedIn && user) {
-      // Only set role if not already set
-      if (!user.publicMetadata?.role) {
-        user.update({ publicMetadata: { role: "user" } });
-      }
-      navigate("/unauthorized");
-    }
-  }, [isSignedIn, user, navigate]);
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
